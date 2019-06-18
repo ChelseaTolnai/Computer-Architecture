f355d5ae23a9f9f7c78836c1df6f9bc6be48270b
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-06-18 13:50:52 -0400

    Step 8: Un-hardcoded the cpu_load() method in cpu.c by updating function, updating main() in ls8.c, and updating cpu.h cpu_load() call.

--- ls8/ls8.c
+++ ls8/ls8.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "cpu.h"
 
 /**
  * Main
  */
-int main(void)
+int main(int argc, char *argv[])
 {
+  if (argc != 2) {
+    fprintf(stderr, "Error - Invalid input; Expected two arguments. (Ex. `./ls8 examples/mult.ls8`)\n");
+    exit(1);
+  }
   struct cpu cpu;
 
   cpu_init(&cpu);
-  cpu_load(&cpu);
+  cpu_load(&cpu, argv[1]);
   cpu_run(&cpu);
 
   return 0;
